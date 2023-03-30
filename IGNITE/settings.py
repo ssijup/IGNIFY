0a5c0059d3d37e18dd001d469109314da92f16a6
siju <ssijup@gmail.com>
2023-03-30 10:19:10 +0530

    third

--- IGNITE/settings.py
+++ IGNITE/settings.py
@@ -95,17 +95,24 @@ WSGI_APPLICATION = 'IGNITE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'march16',
-        'USER':'postgres',
-        'PASSWORD':'2486',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'march16',
+#         'USER':'postgres',
+#         'PASSWORD':'2486',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
